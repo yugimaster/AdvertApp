edbad819f4ef17e9d777c497b92a92bb78140d65
yanliting2001 <liting.yan@zenithnine.com>
2018-10-10 09:58:38 +0800

    save playrecord to usb disk

--- app/src/main/java/com/grandartisans/advert/utils/FileOperator.java
+++ app/src/main/java/com/grandartisans/advert/utils/FileOperator.java
@@ -4,6 +4,7 @@ import android.util.Log;
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileDescriptor;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -225,4 +226,65 @@ public class FileOperator {
         }
         return true;
     }
+
+    /**
+     * 字符串转换成为16进制(无需Unicode编码)
+     * @param str
+     * @return
+     */
+    public static String str2HexStr(String str) {
+        char[] chars = "0123456789ABCDEF".toCharArray();
+        StringBuilder sb = new StringBuilder("");
+        byte[] bs = str.getBytes();
+        int bit;
+        for (int i = 0; i < bs.length; i++) {
+            bit = (bs[i] & 0x0f0) >> 4;
+            sb.append(chars[bit]);
+            bit = bs[i] & 0x0f;
+            sb.append(chars[bit]);
+            // sb.append(' ');
+        }
+        return sb.toString().trim();
+    }
+    /**
+     * 16进制直接转换成为字符串(无需Unicode解码)
+     * @param hexStr
+     * @return
+     */
+    public static String hexStr2Str(String hexStr) {
+
+        String str = "0123456789ABCDEF";
+        char[] hexs = hexStr.toCharArray();
+        byte[] bytes = new byte[hexStr.length() / 2];
+        int n;
+        for (int i = 0; i < bytes.length; i++) {
+            n = str.indexOf(hexs[2 * i]) * 16;
+            n += str.indexOf(hexs[2 * i + 1]);
+            bytes[i] = (byte) (n & 0xff);
+        }
+        return new String(bytes);
+    }
+
+    public static void saveStringToFile(String fileName, String value) {
+        File file = new File(fileName);
+        FileOutputStream fos = null;
+        FileDescriptor fd = null;
+        try {
+            fos = new FileOutputStream(file, false);
+            fd = fos.getFD();
+            fos.write(value.getBytes());
+            fos.flush();
+            fd.sync();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                }
+            }
+        }
+    }
+
 }
